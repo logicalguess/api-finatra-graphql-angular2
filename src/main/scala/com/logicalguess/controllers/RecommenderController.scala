f1782f2bce1d055ab66f1049cf67713d9ea85355
logicalguess <logicalguess@gmail.com>
2016-02-26 16:43:31 -0500

    recommender server

--- src/main/scala/com/logicalguess/controllers/RecommenderController.scala
+++ src/main/scala/com/logicalguess/controllers/RecommenderController.scala
@@ -11,7 +11,6 @@ import com.twitter.finatra.http.Controller
 class RecommenderController @Inject()(recSvc: ALSRecommenderService) extends Controller {
 
   get("/api/recommender/:userId") { request: Request =>
-
     val recommendations = recSvc.getRecommendationsForUser(request.params("userId").toInt)
     recSvc.getItems(recommendations.toList.map { r => r.product })
   }
