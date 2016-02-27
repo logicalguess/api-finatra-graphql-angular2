27d102679e0d9e755acb5ebf87a3c4c5b1a67ad1
logicalguess <logicalguess@gmail.com>
2016-02-26 20:41:45 -0500

    recommender server

--- src/main/scala/com/logicalguess/controllers/RecommenderController.scala
+++ src/main/scala/com/logicalguess/controllers/RecommenderController.scala
@@ -13,6 +13,7 @@ class RecommenderController @Inject()(recSvc: ALSRecommenderService) extends Con
   get("/api/recommender/:userId") { request: Request =>
     val recommendations = recSvc.getRecommendationsForUser(request.params("userId").toInt)
     recSvc.getItems(recommendations.toList.map { r => r.product })
+      .zip(recommendations.map {r => r.rating})
   }
 
 }
