96e3962d0dcd325bb269aaa406f1565c6cafddc1
logicalguess <logicalguess@gmail.com>
2016-02-28 21:55:49 -0500

    recommender trait

--- src/main/scala/com/logicalguess/domain/Item.scala
+++ src/main/scala/com/logicalguess/domain/Item.scala
@@ -2,7 +2,10 @@ package com.logicalguess.domain
 
 import com.twitter.finatra.request.FormParam
 
-case class Item(id: String, title: String, desc: String)
+case class Item(
+  id: String,
+  title: String,
+  desc: String)
 
 case class ItemCreationModel(@FormParam title: String, @FormParam desc: String)
 
