8cafa60c9d0e7096e47c665445ec59b115ec8d54
logicalguess <logicalguess@gmail.com>
2016-02-28 21:14:59 -0500

    recommender trait

--- src/main/scala/com/logicalguess/data/DataProvider.scala
+++ src/main/scala/com/logicalguess/data/DataProvider.scala
@@ -7,12 +7,10 @@ import org.apache.spark.rdd.RDD
  * Holds ratings and a Map which maps IDs to names of products
  */
 trait DataProvider {
-
   protected val ratings: RDD[Rating]
   protected val productNames: Map[Int, String]
 
   def getRatings(): RDD[Rating] = ratings
-
   def getProductNames(): Map[Int, String] = productNames
 }
 
